871d033d185addc86a4f14e8cf28e700a34e11f0
RafaelLeal93 <58343296+RafaelLeal93@users.noreply.github.com>
2021-08-09 11:20:16 +0100

    mobile version added

--- index.php
+++ index.php
@@ -7,106 +7,115 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">    <link href="assets/styles.css" rel="stylesheet">
     <title>Exercicio 2</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link href="assets/styles.css" rel="stylesheet">
   </head>
 
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light mb-4">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <nav class="site-header py-1 float-right m-1 mb-4 ">
-
-      <div class="container d-flex flex-column flex-md-row justify-content-between">
-        <a class=" border-bottom border-danger" href="#">All Products</a>
-        <a class=" border-bottom border-secondary" href="#">Suspension</a>
-        <a class=" border-bottom border-info" href="#">Floor</a>
-        <a class=" border-bottom border-light" href="#">Table</a>
-        <a class=" border-bottom border-success" href="#">Wall</a>
-        <a class=" border-bottom border-dark" href="#">Graphic</a>
-        <a class=" border-bottom border-info" href="#">Furniture</a>
-        <img src="assets/images/loupe.svg" alt="" class="img-svg border-bottom border-dark">
-
-        <form class=""> 
-        <input class=" search border-bottom border-dark" type="search" placeholder="Search" aria-label="Search">
-        </form>
-
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item border-bottom border-danger pr-5 mr-4 pb-1">
+            <a class="" href="#">All Products</a>
+          </li>
+          <li class="nav-item border-bottom border-secondary pr-5 mr-4 pb-1">
+            <a class="" href="#">Suspension</a>
+          </li>              
+          <li class="nav-item border-bottom border-info pr-5 mr-4 pb-1">
+            <a class="" href="#">Floor</a>
+          </li>              
+          <li class="nav-item border-bottom border-light pr-5 mr-4 pb-1">
+            <a class="" href="#">Table</a>
+          </li>                         
+          <li class="nav-item border-bottom border-success pr-5 mr-4 pb-1">
+            <a class="" href="#">Wall</a>
+          </li>              
+          <li class="nav-item border-bottom border-dark pr-5 mr-4 pb-1">
+            <a class="" href="#">Graphic</a>
+          </li>
+          </li>              
+          <li class="nav-item border-bottom border-info pr-5 mr-4 pb-1">
+            <a class="" href="#">Furniture</a>
+          </li>
+          <li class="nav-item border-bottom border-dark pr-5 mr-4">
+            <form class=""> 
+              <input class="search searchbackground" type="search" placeholder="Search" aria-label="Search">
+            </form>              
+          </li>
+          </ul>
       </div>
-
     </nav>
 
+    <div class="container-fluid p-0">
+      <div class="row no-gutters bg-light ">
 
+            <div class="col-12 col-lg-6 no-gutters ">
 
+              <img src="assets/images/main-image.jpg" alt="" class="img-fluid w-100">
+            
+            </div>
+            
+            <div class="col-12 col-lg-6 no-gutters d-flex justify-content-center my-auto ">
 
-    <div class="row no-gutters w-100 h-100 bg-light ">
+              <form>
+                <div class="form-group">
 
-          <div class="col-md no-gutters w-100">
+                  <h1 class="">BOOK A VIRTUAL MEETING</h1>
+                  <div class="redborder">
+                  </div>
+                  <div class="grayborder">
+                    <input type="name" class="form-control namebackground" id="" aria-describedby="nameHelp" placeholder="Enter your Name">
+                  </div>
+                  
+                  <div class="grayborder">
+                    <input type="email" class="form-control emailbackground" id="" aria-describedby="emailHelp" placeholder="Enter your email">
+                  </div>
+                  <div class="grayborder locationwidth">
+                  
+                    <select class="form-control countrybackground">
 
-            <img src="assets/images/main-image.jpg" alt="" class="img-fluid w-100">
-          
-          </div>
-          
-          <div class="col-md no-gutters overlay d-flex justify-content-center my-auto ">
-
-            <form>
-              <div class="form-group w-100 ">
-
-                <h1 class="">BOOK A VIRTUAL MEETING</h1>
-                <div class="redborder">
-                </div>
-                <div class="d-md-flex  grayborder">
-                  <img src="assets/images/user.svg" alt="" class="img-svg">
-
-                  <input type="name" class="form-control" id="" aria-describedby="nameHelp" placeholder=" Enter your Name">
-                </div>
+                        <option>Choose your Country</option>
+                        
+                    </select>
                 
-                <div class="d-md-flex  grayborder">
-                  <img src="assets/images/email.svg" alt="" class="img-svg">
+                  </div>
 
-                  <input type="email" class="form-control" id="" aria-describedby="emailHelp" placeholder="Enter your email">
-                </div>
-                <div class="d-md-flex  w-50 grayborder">
-                
-                  <img src="assets/images/placeholder.svg" alt="" class="img-svg">
+                  <div class="d-md-flex ">
 
-                  <select class="form-control  ">
+                    <div class="w-100 grayborder">
 
-                      <option>Choose your Country</option>
-                      
-                  </select>
+                      <select class="form-control occupationbackground">
+                        <option>Choose your Occupation</option>
+                      </select>
+
+                    </div>
+
+                    <div class="d-md-flex grayborder marginleft w-100">
+                      <i class="mt-4"></i>
+
+                      <input type="company" class="form-control companybackground" id="" aria-describedby="companyHelp" placeholder="Enter your Company">
+
+                    </div>
+                  </div>
+
+                  <button type="button" class="btn mt-3 mb-3  float-right">send now</button>
               
                 </div>
+              </form>
+        
 
-                <div class="d-md-flex">
-                  <img src="assets/images/briefcase.svg" alt="" class="img-svg grayborder">
-                  <div class=" grayborder w-100">
-
-                    <select class="form-control  ">
-                      <option>Choose your Occupation</option>
-                    </select>
-
-                  </div>
-
-                  <div class="d-md-flex grayborder ml-3 w-100">
-                    <img src="assets/images/buildings.svg" alt="" class="img-svg ">
-                    <input type="company" class="form-control " id="" aria-describedby="companyHelp" placeholder="Enter your Company">
-
-                  </div>
-                </div>
-
-                <button type="button" class="btn mt-3 float-right">send now</button>
-            
-              </div>
-            </form>
-      
-
-          </div>
-   
+            </div>
+    
+      </div>
     </div>
-
     <div class="d-md-flex flex-md-equal w-100 mt-1">
-      <div class="mr-1 ">
+      <div class="mr-1">
         <div class="overlay">
           <img src="assets/images/products-cat/1-suspension.jpg" alt="" class="img-fluid w-100">
             <div  class="centered-bottom pl-4 red">
@@ -114,7 +123,7 @@
             </div>
         </div>
       </div>
-      <div class="mr-1 ">
+      <div class="mr-1">
           <div class="overlay">
             <img src="assets/images/products-cat/2-wall.jpg" alt="" class="img-fluid w-100">
             <div  class="centered-bottom pl-4 orange">
@@ -139,7 +148,7 @@
         </div>
       </div>
       <div class="">
-        <div class="overlay ">
+        <div class="overlay">
           <img src="assets/images/products-cat/5-pendant.jpg" alt="" class="img-fluid w-100">
             <div  class="centered-bottom pl-4 mb-3 red ">
               <p>CARTER PENDANT</p>
@@ -149,7 +158,8 @@
       </div>
       
     </div>
-
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
